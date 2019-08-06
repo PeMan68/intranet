a3431e9d1c3ee4e88d16a1628ebbf8ea6a9bd765
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-08-06 13:27:18 +0200

    changed menu

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Demostock</a>
+  <a class="navbar-brand" href="#">Intranet</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,30 +7,44 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="/">Hem</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Produkter
+          Marknad
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/products">Visa produktlista från Sigip</a>
-          <a class="dropdown-item" href="./importproducts">Uppdatera produktlista från Sigip</a>
+			<a class="dropdown-item" href="http://172.16.0.2:10099/INTRANET/SFA/">SFA</a>
+			<div class="dropdown-divider"></div>
+			<a class="dropdown-item" href="http://172.16.0.2:10099/INTRANET/SFA/">SFA Sök kund</a>
+			<a class="dropdown-item" href="http://172.16.0.2:10099/INTRANET/SFA/">SFA Sök Offert</a>
+		</div>
+	</li>
+     <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Adminstration
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href=" http://172.16.0.184/qweb ">QUARTA</a>
+          <a class="dropdown-item" href="http://194.243.72.228">GESTREQ</a>
+          <a class="dropdown-item" href="http://172.16.0.161">Reserved Area</a>
+          <a class="dropdown-item" href="http://172.16.0.183/projects/si5/wiki">Dokumentation SIGIP mm</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="https://po.unifaun.se/ext.po.se.posten">Posten Adresslappar</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+
+	  <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Teknisk Support
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="http://productselection.net">Product Selection</a>
+          <a class="dropdown-item" href="http://gavazzi.se">Nya Gavazzi.se</a>
+          <a class="dropdown-item" href="http://support-carlogavazzi.se">Gamla supportsidan</a>
+        </div>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
