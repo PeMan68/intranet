fa86361847da3a43cede4f97749fee0c4567c966
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-08-07 11:32:49 +0200

    Added Helperfile

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,18 @@
+<?php
+ 
+if (!function_exists('update_table_responsibilites')) {
+    /**
+     * Updates the table responsibilities from actual tasks in table tasks.
+     *
+	 * When a new task is added to tasks, or a task is dropped from tasks,
+	 * the table priorities needs to be updated, so all users have the 
+	 * correct pririty for each available task.
+	 *
+     * @return void
+     *
+     * */
+    function update_table_responsibilites()
+    {
+		//
+    }
+}
