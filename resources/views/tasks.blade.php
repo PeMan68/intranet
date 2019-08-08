59145682e049a2da9e54163b5657498dfbadb6e8
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-08-08 22:21:59 +0200

    moved updates of tasks/responsibilities to separate Controller

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,12 +5,5 @@
 @section('header','Tasks i databasen')
 
 @section('content')
-	@php
-	use App\Helpers\Helper;
-	echo '<h2>Lägg till tasks</h2><br>';
-	add_responsibilites();
-	echo '<h2>Ta bort tasks</h2><br>';
-	delete_responsibilites();
-	
-	@endphp
+
 @endsection
