040151d0909176aedfa5249a5517285770a73418
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-08-25 22:34:30 +0200

    equal cellsize by filling with image. Using class invisible keeps same width

---
+++ resources/views/partials/_kalender.blade.php
@@ -0,0 +1,93 @@
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Kalender</div>
+
+                <div class="card-body">
+				<button>Lägg till</button>
+				<button>Framåt</button>
+				<button>Bakåt</button>
+					<table class="table table-sm table-bordered">
+						<thead>
+							<tr>
+								<th scope="col">
+									Namn
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+								<th scope="col">
+									1/1
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td class="table-dark">
+									Namn
+								</td>
+								<td class="table-success">
+									Sem
+								</td>
+								<td class="table-success">
+									Sem
+								</td>
+								<td class="table-success">
+									Sem
+								</td>
+								<td class="table-success">
+									Sem
+								</td>
+								<td class="table-success">
+									Sem
+								</td>
+								<td class="table-success">
+									Sem
+								</td>
+								<td class="table-success">
+									Sem
+								</td>
+								<td>
+									&nbsp;
+								</td>
+								<td>
+									&nbsp;
+								</td>
+								<td>
+									&nbsp;
+								</td>
+							</tr>
+						</tbody>
+					</table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
