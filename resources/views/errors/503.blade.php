6f4182f5c95d142085219fc63990ad68ae8c8df4
Per Jobbet <per.manholm@carlogavazzi.se>
2019-09-09 11:33:11 +0200

    Changed 503-message

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors::layout')
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+@section('message', __($exception->getMessage() ?: 'Tillfälligt avbrott, intranet uppdateras. Prova igen om en kvart...'))
