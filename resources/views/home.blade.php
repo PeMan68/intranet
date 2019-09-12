d3c176384c9326ffb372d85d90446d0844598507
Per Jobbet <per.manholm@carlogavazzi.se>
2019-09-12 11:54:22 +0200

    Few adjustements on the main layout of homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
 
-@include('partials._chart')
-
-@include('partials._kalender')
+<div class="container">
+	<div class="row pb-3">
+		<div class="col">
+			@include('partials._chart')
+		</div>
+	</div>
+	<div class="row">
+		<div class="col pb-3">
+			@include('partials._kalender')
+		</div>
+	</div>
+</div>
 
 @endsection
