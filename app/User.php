735bc123e69d21a48541349f5ae5b6e8a6608b7d
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-09-22 22:03:30 +0200

    fix storing of json-field

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'surname', 'email', 'password', 'active', 'calendar'
+        'name', 'surname', 'email', 'password', 'active', 'calendar', 'responsibilities',
     ];
 
     /**
