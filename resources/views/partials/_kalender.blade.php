d7cec1faad5153a50cadae321e076d0082a7c54f
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-09-28 19:52:19 +0200

    Added possibility to use color schemes by variables in colors.css

--- resources/views/partials/_kalender.blade.php
+++ resources/views/partials/_kalender.blade.php
@@ -17,21 +17,17 @@
 				<div class="calendar">
 					@for ($date=$start; $date<=$stop; $date=strtotime('+1 day', $date))
 							@if ($date<>$start)
-								@if (date('w', $date)==0)
-									<div class="calendar-item-sunday"> 
-									<div class="dates sunday">
-								@elseif (date('w', $date)==6)
-									<div class="calendar-item-saturday"> 
-									<div class="dates">
+								@if (date('Ymd')==date('Ymd',$date))
+									<div class="calendar-header-today"> 
+								@elseif (date('w', $date)==0 or date('w', $date)==6)
+									<div class="calendar-header-weekend"> 
 								@else
-									<div class="calendar-item"> 
-									<div class="dates">
+									<div class="calendar-header"> 	
 								@endif
 								{{ date('j/n',$date) }}
-								</div>
 							@else
-								<div class="calendar-item">
-								<div>&nbsp;</div>
+								<div class="calendar-header"> 
+							<div>&nbsp;</div>
 							@endif
 						</div>
 					@endfor
@@ -41,39 +37,39 @@
 									<div class="calendar-item"> 
 									<div class="names">{{ $user->name }} {{ $user->surname[0] }}</div>
 								@else
-									@if (date('w', $date)==0)
-										<div class="calendar-item-sunday"> 
-									@elseif (date('w', $date)==6)
-										<div class="calendar-item-saturday"> 
-									@else
-										<div class="calendar-item"> 
-									@endif
+								@if (date('Ymd')==date('Ymd',$date))
+									<div class="calendar-item-today"> 
+								@elseif (date('w', $date)==0 or date('w', $date)==6)
+									<div class="calendar-item-weekend"> 
+								@else
+									<div class="calendar-item"> 
+								@endif
 									@foreach ($activities as $activity)
 										@if ($activity->start <= date('Y-m-d',$date) And $activity->stop >= date('Y-m-d', $date) And $user->id == $activity->user_id)
 											@switch ($activity->calendarcategory_id)
 												@case(1)
 													<a href="{{ route('calendar.edit',$activity->id) }}" data-toggle="tooltip" title="{{$activity->description }}">
-														<div class="activity text-truncate" style="background-color: lightskyblue;">{{ $activity->description }}</div>
+														<div class="activity text-truncate bg-activity-info">{{ $activity->description }}</div>
 													</a>
 													@break
 												@case(2)
 													<a href="{{ route('calendar.edit',$activity->id) }}" data-toggle="tooltip" title="{{$activity->description }}">
-														<div class="activity text-truncate" style="background-color: green;">{{ $activity->description }}</div>
+														<div class="activity text-truncate bg-activity-ledig">{{ $activity->description }}</div>
 													</a>
 													@break
 												@case(3)
 													<a href="{{ route('calendar.edit',$activity->id) }}" data-toggle="tooltip" title="{{$activity->description }}">
-														<div class="activity text-truncate" style="background-color: lightgreen;">{{ $activity->description }}</div>
+														<div class="activity text-truncate  bg-activity-prel">{{ $activity->description }}</div>
 													</a>
 													@break
 												@case(4)
 													<a href="{{ route('calendar.edit',$activity->id) }}" data-toggle="tooltip" title="{{$activity->description }}">
-														<div class="activity text-truncate" style="background-color: orange;">{{ $activity->description }}</div>
+														<div class="activity text-truncate bg-activity-resa">{{ $activity->description }}</div>
 													</a>
 													@break
 												@case(5)
 													<a href="{{ route('calendar.edit',$activity->id) }}" data-toggle="tooltip" title="{{$activity->description }}">
-														<div class="activity" style="background-color: yellow;">{{ $activity->description }}</div>
+														<div class="activity text-truncate bg-activity-sjuk">{{ $activity->description }}</div>
 													</a>
 													@break
 											@endswitch
