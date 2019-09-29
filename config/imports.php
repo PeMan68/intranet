597d3f056b0de91aa459ce66d8fffb2d5d896d8c
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-09-29 12:09:35 +0200

    Setting up importing parameters

---
+++ config/imports.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Path and filenames for imports
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'path_booking' => env('IMPORT_PATH_BOOKING', 'c:\temp\booking'),
+    'file_booking' => env('IMPORT_FILE_BOOKING', 'booking.csv'),
+];
