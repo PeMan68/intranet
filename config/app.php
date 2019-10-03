95c6faddb13caed0cdb9644cc0ccae1d86ba9b81
unknown <per@carlogavazzi.se>
2019-10-03 09:53:57 +0200

    regenerated composer.lock

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 		App\Providers\BladeExtrasServiceProvider::class,
-		Themsaid\Langman\LangmanServiceProvider::class,
     ],
 
     /*
