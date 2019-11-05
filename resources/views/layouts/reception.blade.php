5853f2a53178463e1348762d8fe242d0b254d346
unknown <per@carlogavazzi.se>
2019-11-05 16:48:29 +0100

    basic reception view with visitors

--- resources/views/layouts/reception.blade.php
+++ resources/views/layouts/reception.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<meta http-equiv="refresh" content="600">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -23,7 +23,7 @@
     <link href="{{ asset('css/colors.css') }}" rel="stylesheet">
 	@yield('stylesheets')
 </head>
-<body>
+<body style="background: linear-gradient(#C0D2F4,#1155D3) fixed;">
     <div id="app">
 
 
--- resources/views/reception.blade.php
+++ resources/views/reception.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.reception')
 
 @section('content')
+	<img src="images/CG-RedBlack.png" class="img-fluid float-right m-2" style="width:15%; height:auto;">
+	<p class="display-1 text-center" style="color:rgb(120, 120, 120);">Välkommen till Carlo Gavazzi</p>
+	<hr class="my-4">
+	<p class="display-4 text-center" style="color:#FFFFFF;">Idag besöks vi av</p>
+		<dl class="row display-3" style="color:#FFFFFF;">
+			@foreach ($visitors as $visitor)
+				<dt class="col-sm-2"></dt>
+				<dt class="col-sm-4">{{ $visitor->name }}</dt>
+				<dd class="col-sm-6">{{ $visitor->company }}</dd>
+			@endforeach
+		</dl>
 
-	<p class="display-1 text-center">Välkommen till Carlo Gavazzi</p>
-	<div class="card">
-		<div class="card-header">Besökare {{ date('Y-m-d') }}</div>
-	<ul>
-	@foreach ($visitors as $visitor)
-		<li>{{ $visitor->name }}, {{ $visitor->company }}</li>
-	@endforeach
-	</ul>
-	</div>
-</div>
 @endsection
