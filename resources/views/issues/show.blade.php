49f9920bb49ad481f2f101230b3c938255af7f8f
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2020-02-23 20:24:10 +0100

    F200218-1: Visa att ett ärende är öppet både .index och .show, disabla ärendefält, men tillåt kommentarer.

--- resources/views/issues/show.blade.php
+++ resources/views/issues/show.blade.php
@@ -21,12 +21,24 @@ $(document).ready(function(){
 @section('content')
 <div class="card">
 	<div class="card-header h3">Ärende {{ $issue->ticketNumber }}
+	
 	</div>
 
 	<div class="card-body">
+		@if ($auth_user->id <> $issue->userCurrent_id)
+		<div class="alert alert-info">
+			Ärendet är för närvarande öppet av
+			{{ $issue->userCurrent->name }}
+			{{ $issue->userCurrent->surname }}.
+			Du kan läsa men inte ändra några uppgifter förrän ärendet är ledigt.
+			Det går att lägga till kommentarer.
+		</div>
+		@endif
 		<form action="{{ url('issues', [$issue->id]) }}" method="post" id="issueheader">
 			@method('PUT')
 			@csrf
+			<fieldset @if ($auth_user->id <> $issue->userCurrent_id) disabled @endif>
+
 			<div class="row">
 				<div class="col-md-6">
 					<table style="width: 100%;">
@@ -38,9 +50,13 @@ $(document).ready(function(){
 						{{ $issue->userCreate->surname}}
 						</td></tr>
 						<tr><td><strong>Kundnr:</strong></td>
-							<td><input type="text" value="{{ old('customerNumber', $issue->customerNumber) }}" class="form-control" id="customerNumber" name="customerNumber"></td></tr>
+							<td>
+								<input type="text" value="{{ old('customerNumber', $issue->customerNumber) }}" 
+								class="form-control" id="customerNumber" name="customerNumber">
+							</td>
+						</tr>
 						<tr><td><strong>Kund:</strong></td>
-							<td><input type="text" value="{{ $issue->customer }}" class="form-control" id="customer" name="customer"></td></tr>
+							<td><input type="text" value="{{ $issue->customer }}" class="form-control" id="customer" name="customer" ></td></tr>
 						<tr><td><strong>Kontakt:</strong></td>
 							<td><input type="text" value="{{ $issue->customerName }}" class="form-control" id="customerName" name="customerName"></td></tr>
 						<tr><td><strong>Telefon:</strong></td>
@@ -98,39 +114,41 @@ $(document).ready(function(){
 
 				</div>
 			</div>
-			
+			</fieldset>
 				<hr>
 			<div class="row">
 				<div class="col-md-6">
-					<div>
+					<div class="alert alert-info">
 						@foreach ($followers as $user)
-							<span class="badge badge-pill m-1 font-weight-light shadow bg-info" data-toggle="tooltip" title="{{ $user->name.' '.$user->surname }}">{{ $user->initials() }}</span>
+							<span class="badge badge-pill circle badge-dark font-weight-light" data-toggle="tooltip" title="{{ $user->name.' '.$user->surname }}">{{ $user->initials() }}</span>
 						@endforeach
 						@if ($follow)
 							<a class="btn btn-primary btn-sm m-2" href="{{ route('issues.unfollow', $issue->id) }}" role="button">Sluta följa ärende</a>
 						@else
 							<a class="btn btn-primary btn-sm m-2" href="{{ route('issues.follow', $issue->id) }}">
-								Följ ärende<i class="material-icons white md-18 ml-1" data-toggle="tooltip" title="Följ ärendet för att få mail när det händer något">help</i>
+								Följ ärende<i class="material-icons white md-18 ml-1" data-toggle="tooltip" title="Följ ärendet för att få mail när det händer något" style="vertical-align: middle;">help</i>
 							</a>
 						@endif
 					</div>
 				</div>
 				<div class="col-md-6">
-					<div>
-						@if (is_null($issue->timeCustomercallback))
-							<a class="btn btn-sm btn-primary m-1" href="{{ route('issues.contacted', $issue->id) }}">
-								Kunden är kontaktad
-								<i class="material-icons white md-18 ml-1" data-toggle="tooltip" title="Klicka här för att bekräfta att kund är kontaktad">help</i>
-							</a>
-						@else
-							<i class="material-icons" data-toggle="tooltip" title="Kund kontaktad">how_to_reg</i> 
-							Kund kontaktad: {{ date('Y-m-d H:i', strtotime($issue->timeCustomercallback)) }}
-							<a class="btn btn-sm btn-secondary m-1" href="{{ route('issues.uncontacted', $issue->id) }}">
-								Ångra kund kontaktad
-								<i class="material-icons white md-18 m-1" data-toggle="tooltip" title="Klicka här för ta bort bekräftelsen">help</i>
-							</a>
-						@endif
+					@if (is_null($issue->timeCustomercallback))
+					<div class="alert alert-primary">
+						<a class="btn btn-sm btn-primary m-1" href="{{ route('issues.contacted', $issue->id) }}">
+							Klicka här när kunden är kontaktad
+							<i class="material-icons white md-18 ml-1" data-toggle="tooltip" title="Klicka här för att bekräfta att kund är kontaktad" style="vertical-align: middle;">help</i>
+						</a>
 					</div>
+					@else
+					<div class="alert alert-success">
+						<i class="material-icons align-middle" data-toggle="tooltip" title="Kund kontaktad">how_to_reg</i> 
+						Kund kontaktad: {{ date('Y-m-d H:i', strtotime($issue->timeCustomercallback)) }}
+						<a class="btn btn-sm btn-secondary m-1" href="{{ route('issues.uncontacted', $issue->id) }}">
+							Ångra kund kontaktad
+							<i class="material-icons white md-18 m-1" data-toggle="tooltip" title="Klicka här för ta bort bekräftelsen" style="vertical-align: middle;">help</i>
+						</a>
+					</div>
+					@endif
 				</div>
 			</div>
 		</form>
