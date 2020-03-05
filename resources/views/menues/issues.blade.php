64194deff4706183b9d573f5b546b6f68b8983e7
Per jobbet <per.manholm@carlogavazzi.se>
2020-03-05 09:30:02 +0100

    restructured left side menues

---
+++ resources/views/menues/issues.blade.php
@@ -0,0 +1,7 @@
+@section('nav-left')
+
+<a class="nav-link" href="{{ url('/issues/create/') }}">Nytt ärende</a>
+<a class="nav-link" href="{{ url('/issues/') }}">Mina ärenden</a>
+<span class="border-bottom"></span>
+
+@endsection
