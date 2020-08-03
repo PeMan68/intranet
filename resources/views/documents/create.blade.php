10b926a8452b9f802d1a73c52faaa593ed503733
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2020-08-03 22:25:49 +0200

    Styled form for document upload

--- resources/views/documents/create.blade.php
+++ resources/views/documents/create.blade.php
@@ -3,16 +3,28 @@
 @include('menues.documents')
 
 @section('content')
-<form action="{{ route('documents.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    Kort beskrivning:
-    <br />
-    <input type="text" name="description" />
-    <br /><br />
-    Fil:
-    <br />
-    <input type="file" name="document" />
-    <br /><br />
-    <input type="submit" value=" Ladda upp " />
-</form>
+<div class="card">
+	<div class="card-header">
+		<div class="row">
+			<div class="col-6">
+				<h3>Dokumenthantering - Ladda upp dokument</h3>
+			</div>
+		</div>
+	</div>
+	<div class="card-body">
+		<form action="{{ route('documents.store') }}" method="POST" enctype="multipart/form-data">
+			@csrf
+			<div class="form-group">
+				<label for="description" class="font-weight-bold">Kort beskrivning</label>
+				<input type="text" class="form-control form-control-sm" id="description"  name="description" value="{{ old('description') }}">
+			</div>
+			<div class="form-group">
+				<label for="document" class="font-weight-bold">Fil (Max 2GB)</label>
+				<input type="file" class="form-control form-control-sm" id="document"  name="document" value="{{ old('document') }}" style="height: calc(1.68125rem + 8px);">
+			</div>
+			<div class="form-group">
+				<input type="submit" value=" Ladda upp " />
+			</div>
+		</form>
+	</div>
 @endsection
