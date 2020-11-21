4b2449cccea057c5924b4e54af7ae87fbb76b791
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2020-11-21 19:35:43 +0100

    Enable automatic registration of vue-components

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,12 +19,12 @@ Vue.use(BootstrapVue);
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+const files = require.context('./', true, /\.vue$/i);
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('demoproducts-table', require('./components/DemoproductsTable.vue').default);
-Vue.component('issue-table', require('./components/IssueTable.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('demoproducts-table', require('./components/DemoproductsTable.vue').default);
+// Vue.component('issue-table', require('./components/IssueTable.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
