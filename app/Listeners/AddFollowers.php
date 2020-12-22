36a9abe2781539a69ee8a6c0f2de08cbf13bc62e
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2020-12-22 20:39:13 +0100

    AddFollowers to new issue ok

--- app/Listeners/AddFollowers.php
+++ app/Listeners/AddFollowers.php
@@ -3,10 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\NewIssue;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Task;
-
 class AddFollowers
 {
     /**
@@ -28,14 +25,14 @@ class AddFollowers
     public function handle(NewIssue $event)
     {
         $taskID = $event->issue->task_id;
-		$issueID = $event->issue->id;
 		$task = Task::find($taskID);
         
 		foreach ($task->users as $user) {
 			if ($user->pivot->level == 3) {
-                $event->issue->followers()->attach($user->id);
+                $event->issue->followers()->syncWithoutDetaching($user->id);
             }
         }
+        
 
     }
 }
