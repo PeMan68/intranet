a0636820d25f2361a64ec12d8d1eb9fa7e816a93
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2020-12-25 18:28:43 +0100

    simplified listener

--- app/Listeners/Issues/NotifyFollowersOfUpdatedIssue.php
+++ app/Listeners/Issues/NotifyFollowersOfUpdatedIssue.php
@@ -5,7 +5,6 @@ namespace App\Listeners\Issues;
 use App\Events\Issues\NewIssue;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Task;
-use App\Issue;
 use App\Jobs\Issues\SendEmailAboutNewIssue;
 use App\Jobs\Issues\SendEmailAboutReminder;
 
@@ -30,20 +29,16 @@ class NotifyFollowersOfUpdatedIssue
     public function handle(NewIssue $event)
     {
         $taskID = $event->issue->task_id;
-		$issueID = $event->issue->id;
 		$task = Task::find($taskID);
-        $issue = Issue::find($issueID);
-        
-		// Mail to all followers
-		$followers = $issue->followers;
+		$followers = $event->issue->followers;
         if ($event->urgent) {
             $delayhours = now()->addMinutes(30);
         } else {
             $delayhours = now()->addHours($task->priority->hours);
         }
         foreach ($followers as $user) {
-            SendEmailAboutNewIssue::dispatch($issue, $user->email, $event->urgent);
-			SendEmailAboutReminder::dispatch($issue, $user->email, $event->urgent)->delay($delayhours);
+            SendEmailAboutNewIssue::dispatch($event->issue, $user->email, $event->urgent);
+			SendEmailAboutReminder::dispatch($event->issue, $user->email, $event->urgent)->delay($delayhours);
         }
     }
 }
