50b0da7249e7a294a8aa6283410eebbe6294a0ae
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2021-02-13 15:45:49 +0100

    Check active users

---
+++ app/Http/Middleware/LastUserActivity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+
+class LastUserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            $expiresAt = now()->addMinutes(60);
+            Cache::put('user-is-online-' . Auth::user()->id, true, $expiresAt);
+        }return $next($request);
+    }
+}
