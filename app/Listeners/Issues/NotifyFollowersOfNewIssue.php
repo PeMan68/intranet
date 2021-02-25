5eb778a54ac28296a6255a2b5ca8a475378fae17
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2021-02-25 19:57:09 +0100

    documentation

--- app/Listeners/Issues/NotifyFollowersOfNewIssue.php
+++ app/Listeners/Issues/NotifyFollowersOfNewIssue.php
@@ -27,25 +27,26 @@ class NotifyFollowersOfNewIssue
      */
     public function handle(NewIssue $event)
     {
-		foreach ($event->issue->task->users as $user) {
-			if ($user->pivot->level == 3) {
+        // Sync so all with level === 3 are followers.
+        foreach ($event->issue->task->users as $user) {
+            if ($user->pivot->level == 3) {
                 $event->issue->followers()->syncWithoutDetaching($user->id);
             }
         }
 
-		$followers = $event->issue->followers;
+        $followers = $event->issue->followers;
         if ($event->urgent) {
             $delayReminder = nextWorkingHour(now()->addMinutes(setting('time_reminder_urgent_issue')));
         } else {
             $delayReminder = nextWorkingHour(now()->addHours($event->issue->task->priority->hours));
         }
-        $delay=nextWorkingHour();
+        $delay = nextWorkingHour();
         if (cache($event->issue->ticketNumber)) {
             $delay = nextWorkingHour($delay->addMinutes(setting('time_disable_update_job')));
         }
         foreach ($followers as $user) {
             SendEmailAboutNewIssue::dispatch($event->issue, $user->email, $event->urgent)->delay($delay);
-			SendEmailAboutReminder::dispatch($event->issue, $user->email, $event->urgent)->delay($delayReminder);
+            SendEmailAboutReminder::dispatch($event->issue, $user->email, $event->urgent)->delay($delayReminder);
         }
     }
 }
