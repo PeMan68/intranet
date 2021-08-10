62d2c5abe88a1563af475a647e8aab08e0e199c8
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2021-08-10 11:28:56 +0200

    Updated link

--- resources/views/posten.blade.php
+++ resources/views/posten.blade.php
@@ -12,18 +12,7 @@
 		Lösenord: QTS856JF</br>
 		</p>
 		<p>
-			<a class="btn btn-secondary" target="_blank" href="https://po.unifaun.se/ext.po.se.posten " role="button">Gå till Posten</a>
-		</p>
-		<p>
-			<div class="alert alert-info">
-				<p>2019-09-02</p>
-				<p>OBS! Utskrifterna funkar ej med Firefox efter Firefox uppdaterats nyligen. 
-				<ul>
-					<li>Använd Internet Explorer för utskrift av adresslapp.</li>
-					<li>Använd 32-bitars java för Internet Explorer.</li>
-				</ul>
-				</p>
-			</div>
+			<a class="btn btn-secondary" target="_blank" href="https://www.unifaunonline.se/jsapp/uo/start" role="button">Gå till sida</a>
 		</p>
 	</div>
 @endsection
