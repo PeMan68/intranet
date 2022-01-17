30b49f0bb93f919f0bb83ed48bceb8d1b557e0f8
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2022-01-17 22:22:31 +0100

    Layout with details

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -21,18 +21,49 @@
                         </table>
                     </b-card>
                 </div>
+                <div class="card-body">
+                    <b-card title="Ersättningsprodukter">
+                        @if (count($product->replacements)==0)
+                        Inga ersättningsprodukter registerade
+                    @else
+                        <table class="table">
+                            @foreach ($product->replacements as $item)
+                               <tr>
+                                <td><a href="{{ url('products', $item->id) }}">{{ $item->item }}</a></td>   
+                                <td>{{ $item->item_description_swe }}</td>
+                                <td>{{ $item->pivot->comment }}</td>
+
+                                </tr> 
+                            @endforeach
+                        </table>
+                        
+                    @endif
+                    </b-card>
+    
+                </div>
             </div>
             <div class="col-lg-6">
                 <div class="card-body">
-                    <b-card title="Demolager">
+                    <b-card title="Demolager för {{ $product->item }} ">
                         @if (count($product->demoproduct)==0)
-                            Inga produkter registerade på något Demolager
+                        Inga produkter registerade på något Demolager
                         @else
-                            {{$product->demoproduct[0]}}   
+                        <table class="table">
+                            @foreach ($product->demoproduct as $item)
+                            <tr>
+                                <td>{{ $item->status->description }}</td>
+                                <td>{{ $item->location->path() }}</td>
+                                <td>{{ $item->comment }}</td>
+                            </tr>
+                            
+                            @endforeach   
+                        </table>
+                        <b-button href="/demoproducts?filter={{ $product->item }}">Detaljer</b-button>
                         @endif
                     </b-card>
                 </div>
             </div>
         </div>
+
     </div>
 @endsection
