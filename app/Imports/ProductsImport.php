8e364ac6263cbca9c77860dac3db27ad9c1eef46
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2022-01-18 22:10:35 +0100

    import form excel

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -21,24 +21,33 @@ class ProductsImport implements WithStartRow, OnEachRow, WithChunkReading
     public function onRow(Row $row)
     {
         $row = $row->toArray();
-
+        
+        // Update timestamp for price only if price is changed
+        $product = Product::where('item', $row[2])->get();
+        if (!is_null($product)) {
+            // Item exists
+            if($row[4] <> $product->listprice) {
+                // Price has changed
+                $product->update('price_date', now());
+            }
+        }
         $product = Product::updateOrCreate([
             'item' => $row[2]
         ], [
-            'item'                  => $row[2],
-            'item_description_swe'  => $row[3],
-            'transferprice'         => $row[5],
-            'currency'              => $row[6],
-            'listprice'             => $row[4],
-            'group'                 => $row[1],
-            'family'                => $row[10],
-            'subfamily'             => $row[11],
-            'safety'                => $row[7],
-            'sourcing'              => $row[17],
-            'status'                => $row[18],
-            'abc'                   => $row[0],
-            'ean'                   => $row[9],
-            'enummer'               => $row[12],
+            'item'                  => $row['Artikel'],
+            'item_description_swe'  => $row['Benämning'],
+            'transferprice'         => $row['Transferprice'] ?? $row['TP'],
+            'currency'              => $row['Valuta'],
+            'listprice'             => $row['Listpris'],
+            'group'                 => $row['PG'],
+            'family'                => $row['Familj'] ?? $row['FAM'],
+            'subfamily'             => $row['Subfamilj'] ?? $row['SUB'],
+            'safety'                => $row['Safety'],
+            'sourcing'              => $row['Sourcing'],
+            'status'                => $row['Status'],
+            'abc'                   => $row['ABC'],
+            'ean'                   => $row['ean'],
+            'enummer'               => $row['enummer'] ?? $row['E-nummer'],
         ]);
     }
 
