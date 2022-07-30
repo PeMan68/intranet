8ea561665e61fe998ac06bcd0547968268039ffd
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2022-07-30 11:44:07 +0200

    Updated form

--- resources/views/admin/images/create.blade.php
+++ resources/views/admin/images/create.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<div class='h1'>
+    Lägg till bild till receptionsskärm
+</div>
+
+<div class="alert alert-info h5">
+    <strong>Information</strong>
+    <p>
+        Lägg till en bild till bildspelet som visas på receptionsskärm.<br>
+    Bilden är den högra delen av skärmen    </p>
 <form action="{{ route('admin.images.store') }}" method="POST" enctype="multipart/form-data">
     @csrf
-    Book title:
-    <br />
-    <input type="text" name="title" />
-    <br /><br />
-    Logo:
+    Bild (max 2 MB):
     <br />
     <input type="file" name="image" />
     <br /><br />
