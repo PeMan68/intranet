37f64ae31992932a83f7d0f5749680e9aa945a1e
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2023-01-12 10:06:19 +0100

    Cleanup

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -2,7 +2,6 @@
 
 namespace App\Exports;
 
-use App\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
